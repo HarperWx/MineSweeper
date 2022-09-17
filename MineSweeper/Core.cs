1c18262993e628d7e8b70419d23b1d5b486951fe
HarperWx <113521969+HarperWx@users.noreply.github.com>
2022-09-17 22:37:29 +0800

    Add files via upload

---
+++ MineSweeper/Core.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    class Core
+    {
+        #region <<<變數>>>
+        //遊戲規格
+        public int _cols;
+        public int _rows;
+        public int _mines;
+
+        //遊戲輸入座標
+        public int _x = 0;
+        public int _y = 0;
+
+        //地雷陣列
+        public int[] _minesArr;
+        public MapArr[,] _mapArr;
+        public struct MapArr
+        {
+            public bool isMarked;
+            public bool isOpen;
+            public bool isMine;
+            public int toCount;
+        };
+        readonly Timer _timer;
+
+        //計分變數
+        int _flags;
+        int _blocks;
+        #endregion
+
+        #region<<<屬性>>>
+        public int Columns
+        {
+            get
+            {
+                return _cols;
+            }
+            set
+            {
+                _cols = value;
+            }
+        }
+        public int Rows
+        {
+            get
+            {
+                return _rows;
+            }
+            set
+            {
+                _rows = value;
+            }
+        }
+        public int Mines
+        {
+            get
+            {
+                return _mines;
+            }
+            set
+            {
+                _mines = value;
+            }
+        }
+        public int Time { get; set; }
+        #endregion
+
+        #region<<<事件>>>
+        public EventHandler OnTick;
+        public EventHandler Win;
+        public EventHandler ToOpen;
+        public EventHandler Explode;
+        public EventHandler Restart;
+        #endregion
+
+        public Core()
+        {
+            _timer = new Timer
+            {
+                Interval = 1000
+            };
+            _timer.Tick += TickTimer;
+        }
+        public void Start()
+        {
+            Time = 0;
+            _timer.Start();
+            _blocks = 0;
+            _flags = 0;
+            CreateMines();
+            PlaceMines();
+            GetResult();
+            Cheating();
+        }
+        private void TickTimer(object sender, EventArgs e)
+        {
+            OnTick(sender, e);
+            Time++;
+        }
+        public void CheckEmpty(int _x, int _y)
+        {
+            while (_mapArr[_y, _x].toCount !=0 || _mapArr[_y, _x].isMine == true)
+            {
+                _blocks = 0;
+                _flags = 0;
+                CreateMines();
+                PlaceMines();
+                GetResult();
+                Cheating();
+                Restart(null, null);
+            }
+        }
+        public void CreateMines()
+        {
+            _minesArr = new int[_mines];
+            Random random = new Random();
+            bool used;
+            for (int i = 0; i<_mines; i++)
+            {
+                do
+                {
+                    _minesArr[i] = random.Next() % (_cols * _rows);
+                    used = false;
+
+                    for (int k = 0; k < i; k++)
+                    {
+                        if (_minesArr[k] == _minesArr[i])
+                        {
+                            used = true;
+                            break;
+                        }
+                    }
+                } while (used);
+            }
+        }
+        public void PlaceMines()
+        {
+            _mapArr = new MapArr[_rows,_cols];
+           
+            for (int i = 0; i < _mines; i++)
+            {
+                int iy = _minesArr[i] / _cols;
+                int ix = _minesArr[i] % _cols;
+                _mapArr[iy, ix].isMine = true;
+            }
+        }
+        public int CountMines(int _x, int _y)
+        {
+            int _rst = 0;
+            for(int iy =0; iy<3; iy++)
+            {
+                for(int ix = 0; ix<3; ix++)
+                {
+                    int trans_rows = _y - 1 + iy;
+                    int trans_cols = _x - 1 + ix;
+
+                    if (trans_cols == _x & trans_rows == _y) continue;
+                    if (trans_cols < 0 || trans_rows < 0) continue;
+                    if (trans_cols >= _cols || trans_rows >= _rows) continue;
+
+                    if (_mapArr[trans_rows, trans_cols].isMine == true)
+                        _rst++;
+                }
+            }
+            return _rst;
+        }
+        public void Cheating()
+        {
+            for (int i =0; i<_rows; i++)
+            {
+                for(int j = 0; j < _cols; j++)
+                {
+                    if (_mapArr[i, j].isMine == true)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public void GetResult()
+        {
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _cols; j++)
+                {
+                    _mapArr[i, j].toCount = CountMines(j, i);
+                }
+            }
+        }
+        public void Judgement(int _x, int _y, bool _isFlag)
+        {
+            if (_isFlag == true)
+            {
+                FlagMode(_x, _y);
+            }
+            else
+            {
+                ButtonRevealed(_x, _y);
+            }
+            WinCondition();
+            Winning();
+        }
+        public void FlagMode(int _x, int _y)
+        {
+                if (_mapArr[_y, _x].isMarked == true)
+                {
+                    if (_mapArr[_y, _x].isMine == true)
+                    {
+                        _flags--;
+                    }
+                    _mapArr[_y, _x].isMarked = false;
+                }
+                else
+                {
+                    if (_mapArr[_y, _x].isMine == true)
+                    {
+                        _flags++;
+                    }
+                    _mapArr[_y, _x].isMarked = true;
+                }
+        }
+        public void ButtonRevealed(int _x, int _y)
+        {
+            if (_mapArr[_y, _x].isMine == true)
+            {
+                Explode(null, null);
+            }
+            else
+            {
+                OpenBlocks(_x, _y);
+            }
+        }
+        public void OpenBlocks(int _x, int _y)
+        {
+            if (_mapArr[_y, _x].isOpen == false)
+            {
+                _mapArr[_y, _x].isOpen = true;
+                ToOpen((Object)(_x + _y * _cols), null);
+                if (_mapArr[_y, _x].toCount == 0)
+                {
+                    Spread(_x - 1, _y - 1);
+                    Spread(_x - 0, _y - 1);
+                    Spread(_x + 1, _y - 1);
+                    Spread(_x - 1, _y - 0);
+                    Spread(_x + 1, _y - 0);
+                    Spread(_x - 1, _y + 1);
+                    Spread(_x - 0, _y + 1);
+                    Spread(_x + 1, _y + 1);
+                }
+            }
+        }
+        public void Spread(int _x, int _y)
+        {
+            if (_x >= 0 && _y >= 0)
+            {
+                if (_x < (_mapArr.GetLength(1)) && _y < _mapArr.GetLength(0))
+                {
+                    if(_mapArr[_y, _x].isMine == false)
+                    {
+                        OpenBlocks(_x, _y);
+                    }
+                }
+            }
+        }
+        public void WinCondition()
+        {
+            _blocks = 0;
+            for (int iy = 0; iy < _rows; ++iy)
+            {
+                for (int ix = 0; ix < _cols; ++ix)
+                {
+                    if (_mapArr[iy, ix].isOpen == true)
+                    {
+                        _blocks++;
+                    }
+                }
+            }
+        }
+        public void Winning()
+        {
+            if ((_blocks + _flags == (_cols*_rows))||(_blocks == (_cols * _rows - _mines)))
+            {
+                Win(null, null);
+                _blocks = 0;
+                _flags = 0;
+            }
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ MineSweeper/Display.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class Display : Form
+    {
+        public int _minestext;
+        public int _cols;
+        public int _rows;
+        public int _width;
+        public int _height;
+        public bool isFirstClick;
+        public bool isFlag;
+
+        readonly Core Core;
+        Label labelNum;
+        Label[] labelMine;
+        Button[] btns;
+
+        public Display()
+        {
+            Core = new Core();
+            InitializeComponent();
+            Core.Win += Core_Win;
+            Core.OnTick += Core_OnTick;
+            Core.Explode += Core_Explode;
+            Core.ToOpen += Core_ToOpen;
+            Core.Restart += Core_Restart;
+            _width = 30;
+            _height = 30;
+        }
+        private void GameInfoInput()
+        {
+            Core.Columns = Convert.ToInt32(textCols.Text);
+            Core.Rows = Convert.ToInt32(textRows.Text);
+            Core.Mines = Convert.ToInt32(textMines.Text);
+            _cols = Core.Columns;
+            _rows = Core.Rows;
+            _minestext = Core.Mines;
+            labelMines.Text = _minestext.ToString();
+        }
+        private void ButtonCreate()
+        {
+            btns = new Button[_cols * _rows];
+            for (int y = 0; y < _rows; ++y)
+            {
+                for (int x = 0; x < _cols; ++x)
+                {
+                    int i = x + y * _cols;
+                    btns[i] = new Button
+                    {
+                        BackColor = Color.DarkGray,
+                        FlatStyle = FlatStyle.Flat
+                    };
+                    btns[i].FlatAppearance.BorderColor = Color.LightGray;
+                    btns[i].Location = new Point(_width * x, _height * y);
+                    btns[i].Tag = i;
+                    btns[i].Size = new Size(_width, _height);
+                    btns[i].TabStop = false;
+                    btns[i].MouseDown += new MouseEventHandler(ClickMode);
+                }
+            }
+            PanelBlocks.Controls.AddRange(btns);
+
+        }
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            isFirstClick = true;
+            GameInfoInput();
+            Core.Start();
+            PanelBlocks.Controls.Clear();
+            ButtonCreate();
+            PanelBlocks.Enabled = true;
+        }
+        private void ClickMode(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+            int y = (int)btn.Tag / _cols;
+            int x = (int)btn.Tag % _cols;
+
+            if (e.Button == MouseButtons.Right)
+            {
+                if (btn.Text != "？")
+                {
+                    isFlag = true;
+                    Core.Judgement(x, y, isFlag);
+                    RightClickMode(btn, x, y);
+                }
+                else
+                {
+                    btn.Text = null;
+                }
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                if (isFirstClick == true)
+                {
+                    isFirstClick = false;
+                    Core.CheckEmpty(x, y);
+                }
+
+                isFlag = false;
+                if (Core._mapArr[y, x].isMarked == false)
+                {
+                    Core.Judgement(x, y, isFlag);
+                }
+            }
+        }
+        private void RightClickMode(Button btn, int x, int y)
+        {
+            if (Core._mapArr[y, x].isMarked == true)
+            {
+                btn.BackColor = Color.DarkRed;
+                _minestext--;
+                labelMines.Text = _minestext.ToString();
+            }
+            else
+            {
+                btn.Text = "？";
+                btn.Font = new Font("微軟正黑體", 16F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(136)));
+                btn.BackColor = Color.DarkGray;
+                _minestext++;
+                labelMines.Text = _minestext.ToString();
+            }
+        }
+        private void ShowNum(int x, int y)
+        {
+            btns[x + y * _cols].Visible = false;
+            labelNum = new Label
+            {
+                Font = new Font("微軟正黑體", 16F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(136))),
+                Location = new Point(_width * x, _height * y),
+                Size = new Size(_width,  _height),
+                Text = Core.CountMines(x, y).ToString(),
+                TextAlign = ContentAlignment.MiddleCenter,
+                BorderStyle = BorderStyle.Fixed3D
+            };
+            PanelBlocks.Controls.Add(labelNum);
+            switch (Convert.ToInt32(labelNum.Text) % 3)
+            {
+                case 1:
+                labelNum.ForeColor = Color.DarkBlue;
+                break;
+                case 2:
+                labelNum.ForeColor = Color.DarkGreen;
+                break;
+                case 0:
+                labelNum.ForeColor = Color.DarkRed;
+                break;
+            }
+            if(Convert.ToInt32(labelNum.Text) == 0)
+            {
+                labelNum.Text = " ";
+            }
+        }
+        private void ShowallMines()
+        {
+            labelMine = new Label[Core._minesArr.Length];
+            for (int i = 0; i < Core._minesArr.Length; i++)
+            {
+                labelMine[i] = new Label
+                {
+                    Font = new Font("微軟正黑體", 16F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(136))),
+                    Size = new Size(_width, _height),
+                    Text = "#",
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    BorderStyle = BorderStyle.Fixed3D
+                };
+                labelMine[i].Location = btns[Core._minesArr[i]].Location;
+                PanelBlocks.Controls.AddRange(labelMine);
+                btns[Core._minesArr[i]].Visible = false;
+            }
+        }
+        private void Core_Win(object sender, EventArgs e)
+        {
+            Core.Stop();
+            MessageBox.Show("You Win !!!");
+            PanelBlocks.Enabled = false;
+        }
+        private void Core_OnTick(object sender, EventArgs e)
+        {
+            labelTime.Text = Core.Time.ToString();
+        }
+        private void Core_Restart(object sender, EventArgs e)
+        {
+            labelNum = null;
+            labelMine = null;
+        }
+        private void Core_ToOpen(object sender, EventArgs e)
+        {
+            int position = (int)sender;
+            int x = position % _cols;
+            int y = position / _cols;
+            ShowNum(x, y);
+        }
+        private void Core_Explode(object sender, EventArgs e)
+        {
+            ShowallMines();
+            Core.Stop();
+            MessageBox.Show("Boom !!!");
+            PanelBlocks.Enabled = false;
+        }
+    }
+}
